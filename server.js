2066df59747c570bdb6ad43b7a5582750e8bab62
Shivamteotia96 <chaudharyshivam82186@gmail.com>
2025-03-17 22:25:02 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Connect to Database
+connectDB();
+
+// Routes
+app.use('/api/deliveries', require('./routes/deliveryRoutes'));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
